b6174b354b03187bd8f4b9631833bd4e8cbe07a8
jimpacyga <94812174+jimpacyga@users.noreply.github.com>
2021-11-24 10:49:45 -0500

    Stuff (#2)
    * new 'stuff'
    
    * added arms
    
    * Added test project "TestingGitHub.Tests" and a single test for the Person constructor.
    
    Co-authored-by: jpacyga <jpacyga@uwm.com>

---
+++ TestingGitHub.Tests/PersonTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using TestingGitHub;
+
+namespace TestingGitHub.Tests
+{
+    [TestFixture]
+    public class PersonTests
+    {
+        [Test]
+        public void PersonCtor_Always_CreatesPerson()
+        {
+            // Arrange
+            int age = 42;
+            string hairColor = "brown";
+            string name = "bobby";
+            int numberOfArms = 2;
+
+            // Act
+            Person p = new Person
+            {
+                HairColor = hairColor,
+                Age = age,
+                Name = name,
+                NumberOfArms = numberOfArms
+            };
+
+            // Assert
+            Assert.IsNotNull(p);
+            Assert.AreEqual(age, p.Age);
+            Assert.AreEqual(hairColor, p.HairColor);
+            Assert.AreEqual(name, p.Name);
+            Assert.AreEqual(numberOfArms, p.NumberOfArms);
+        }
+    }
+}
--- TestingGitHub/Person.cs
+++ TestingGitHub/Person.cs
@@ -1,13 +1,13 @@
 ﻿
 namespace TestingGitHub
 {
-    internal class Person
+    public class Person
     {
         public int Age { get; set; }
 
-        public string HairColor { get; set; }
+        public string HairColor { get; set; } = null;
 
-        public string Name { get; set; }
+        public string Name { get; set; } = null;
 
         public int NumberOfArms { get; set; }
     }
